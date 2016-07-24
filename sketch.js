d7a1765acbe972ced155494fbb2599e22c0a26cd
silverroxy79 <vabro001@mail.goucher.edu>
2016-07-24 17:36:14 -0400

    Project1-MouseActions
    6th commit

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ function preload(){
  vancolor=loadImage("assets/vancolor.jpg")
  vangreen=loadImage("assets/vangreen.jpg")
  servercolor=loadImage("assets/servercolor.jpg")
- severgreen=loadImage("assets/servergreen.jpg")
+ servergreen=loadImage("assets/servergreen.jpg")
  foodcolor=loadImage("assets/foodcolor.jpg")
  foodgreen=loadImage("assets/foodgreen.jpg")
 }
@@ -13,17 +13,40 @@ var h=480
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
+  fullcolor.resize(windowWidth*0.6,0)
+  vancolor.resize(windowWidth*0.6,0)
+  vangreen.resize(windowWidth*0.6,0)
+  servercolor.resize(windowWidth*0.6,0)
+  servergreen.resize(windowWidth*0.6,0)
+  foodcolor.resize(windowWidth*0.6,0)
+  foodgreen.resize(windowWidth*0.6,0)
 }
 
 function draw() {
   background(0)
   imageMode("center")
-  image(fullcolor,windowWidth/2,windowHeight/2,w,h)
-  
-  if(mouseX > 475 && mouseY > 375 && mouseX < 575 && mouseY < 475){
-    image(foodcolor,windowWidth/2,windowHeight/2,w,h)
+  image(fullcolor,windowWidth/2,windowHeight/2)
+  if(mouseX > windowWidth*0.44 && mouseY > windowHeight*0.51 && mouseX < windowWidth*0.47 && mouseY < windowHeight*0.59){
+    image(foodcolor,windowWidth/2,windowHeight/2)
   }
-  if(mouseX > 475 && mouseY > 375 && mouseX < 575 && mouseY < 475 && mouseIsPressed){
-      image(foodgreen,windowWidth/2,windowHeight/2,w,h)
+  
+  if(mouseX > windowWidth*0.44 && mouseY > windowHeight*0.51 && mouseX < windowWidth*0.47 && mouseY < windowHeight*0.59 && mouseIsPressed){
+      image(foodgreen,windowWidth/2,windowHeight/2)
+      }
+  
+  if(mouseX > windowWidth*0.49 && mouseY > windowHeight*0.28 && mouseX < windowWidth*0.47 && mouseY < windowHeight*0.59){
+    image(servercolor,windowWidth/2,windowHeight/2)
+  }
+  
+  if(mouseX > windowWidth*0.49 && mouseY > windowHeight*0.28 && mouseX < windowWidth*0.55 && mouseY < windowHeight*0.56 && mouseIsPressed){
+      image(servergreen,windowWidth/2,windowHeight/2)
+      }
+      
+      if(mouseX > windowWidth*0.49 && mouseY > windowHeight*0.28 && mouseX < windowWidth*0.47 && mouseY < windowHeight*0.59){
+    image(vancolor,windowWidth/2,windowHeight/2)
+  }
+  
+  if(mouseX > windowWidth*0.49 && mouseY > windowHeight*0.28 && mouseX < windowWidth*0.55 && mouseY < windowHeight*0.56 && mouseIsPressed){
+      image(vangreen,windowWidth/2,windowHeight/2)
       }
     }
