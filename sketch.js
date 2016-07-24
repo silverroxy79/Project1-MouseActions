03571cb47b84170f1a2e4690ad8372b64c6376d6
silverroxy79 <vabro001@mail.goucher.edu>
2016-07-24 09:21:10 -0400

    Project1-Mouse Actions
    1st commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function preload(){
+ fullcolor=loadImage("assets/fullcolor.jpg")
+ vancolor=loadImage("assets/vancolor.jpg")
+ vangreen=loadImage("assets/vangreen.jpg")
+ servercolor=loadImage("assets/servercolor.jpg")
+ severgreen=loadImage("assets/servergreen.jpg")
+ foodcolor=loadImage("assets/foodcolor.jpg")
+ foodgreen=loadImage("assets/foodgreen.jpg")
+}
+
+var w=windowWidth*0.8
+var h=windowHeight*0.8
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+}
+
+function draw() {
+  background(0)
+  image(fullcolor,windowWidth/2,windowHeight/2,w,h)
+}
